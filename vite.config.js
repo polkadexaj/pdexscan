7b455770f185b9a534dc0ffc4d7e921600c56dce
polkadexaj <ajeesh@polkadex.ee>
2026-05-04 21:39:52 -0700

    Add API proxy for local development

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,12 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   server: {
     port: 3000,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3001',
+        changeOrigin: true
+      }
+    }
   },
   build: {
     target: 'esnext'
